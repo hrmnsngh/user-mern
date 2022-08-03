dadc8311017ba9540633aad8b0892fc59c4da726
Harman Singh <36654923+hrmnsngh@users.noreply.github.com>
2022-08-03 22:28:05 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+import UsersList from './users/pages/UsersList';
+import NewPlace from './places/pages/NewPlace';
 
 function App() {
+  const [users, setUser] = useState([{ id: '11111', image:'https://picsum.photos/200/300?random=1', name: 'Harman Singh', placeCount: 3 },
+  { id: '11112', image:'https://picsum.photos/200/300?random=2', name: 'Vishal Kumar', placeCount: 2  },
+  { id: '11113', image:'https://picsum.photos/200/300?random=3', name: 'Sarthak Bansal', placeCount: 2  },
+  { id: '11114', image:'https://picsum.photos/200/300?random=4', name: 'Sujay Das', placeCount: 3  }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <div className="header">
+        <header>
+          <h1>User location App</h1>
+        </header>
+      </div>
+      <Router>
+        <Switch>
+          <Route path='/users' exact>
+            <UsersList users={users} />
+          </Route>
+          <Route path='/newplace' exact>
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </Router>
     </div>
   );
 }
